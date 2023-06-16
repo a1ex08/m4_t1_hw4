9b3430812c8cda2f4a4edf2c9c7ce4a0ff02e7b7
Alexandr Chizhov <a1ex.chizhov@yandex.ru>
2023-06-16 14:23:50 +0700

    Сравниваем каждый символ с одинаковым порядковым номером с начала и конца. Когда найдено несовпадение изменяем res на False и прирываем цикл. Если несовпадений нет, res остаётся True.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def pal_check(check_str):
     res = True
-    check_str = list(check_str)
+    # Сравниваем каждый символ с одинаковым порядковым номером с начала и конца. 
+    # Когда найдено несовпадение изменяем res на False и прирываем цикл. Если несовпадений нет, res остаётся True
     for char in range(len(check_str)):
         if check_str[char] == check_str[-char - 1] and res == True:
             pass
@@ -10,8 +11,10 @@ def pal_check(check_str):
         
     return res
 
+# Строка палиндром.
 ent_str = 'лепсспел'
 print(ent_str, pal_check(ent_str))
 
+# Строка не палиндром
 ent_str = 'helloworld'
 print(ent_str, pal_check(ent_str))
